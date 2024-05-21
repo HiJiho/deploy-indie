4246518a1b23416a69ff62544c7878a4d2d0d0e4
김지호 <jiho001108@gmail.com>
2024-05-21 14:00:13 +0900

    Update delete-user.ts

--- cypress/support/delete-user.ts
+++ cypress/support/delete-user.ts
@@ -3,7 +3,8 @@
 // npx ts-node -r tsconfig-paths/register ./cypress/support/delete-user.ts username@example.com,
 // and that user will get deleted
 
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+// import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { Prisma } from "@prisma/client";
 import { installGlobals } from "@remix-run/node";
 
 import { prisma } from "~/db.server";
@@ -11,27 +12,27 @@ import { prisma } from "~/db.server";
 installGlobals();
 
 async function deleteUser(email: string) {
-  if (!email) {
-    throw new Error("email required for login");
-  }
-  if (!email.endsWith("@example.com")) {
-    throw new Error("All test emails must end in @example.com");
-  }
+	if (!email) {
+		throw new Error("email required for login");
+	}
+	if (!email.endsWith("@example.com")) {
+		throw new Error("All test emails must end in @example.com");
+	}
 
-  try {
-    await prisma.user.delete({ where: { email } });
-  } catch (error) {
-    if (
-      error instanceof PrismaClientKnownRequestError &&
-      error.code === "P2025"
-    ) {
-      console.log("User not found, so no need to delete");
-    } else {
-      throw error;
-    }
-  } finally {
-    await prisma.$disconnect();
-  }
+	try {
+		await prisma.user.delete({ where: { email } });
+	} catch (error) {
+		if (
+			error instanceof Prisma.PrismaClientKnownRequestError &&
+			error.code === "P2025"
+		) {
+			console.log("User not found, so no need to delete");
+		} else {
+			throw error;
+		}
+	} finally {
+		await prisma.$disconnect();
+	}
 }
 
 deleteUser(process.argv[2]);
