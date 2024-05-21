2dcba5b955aa7db322b29f880f1cc2de5b2bd668
김지호 <jiho001108@gmail.com>
2024-05-21 17:13:51 +0900

    Update swap size

--- start.sh
+++ start.sh
@@ -5,13 +5,30 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-# allocate swap space
+# # allocate swap space
+# fallocate -l 512M /swapfile
+# chmod 0600 /swapfile
+# mkswap /swapfile
+# echo 10 > /proc/sys/vm/swappiness
+# swapon /swapfile
+# echo 1 > /proc/sys/vm/overcommit_memory
+
+# npx prisma migrate deploy
+# npm run start
+
+# Setup 512MB of space for swap and set permissions and turn on swapmode
 fallocate -l 512M /swapfile
 chmod 0600 /swapfile
 mkswap /swapfile
 echo 10 > /proc/sys/vm/swappiness
 swapon /swapfile
-echo 1 > /proc/sys/vm/overcommit_memory
 
+# Run migrations
 npx prisma migrate deploy
+
+# Turn off swap mode and remove swap directory
+swapoff /swapfile
+rm /swapfile
+
+# Finally start the app
 npm run start
