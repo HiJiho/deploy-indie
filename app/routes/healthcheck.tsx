83539f88ce22c95132b489a1d2b79c98f6215684
김지호 <jiho001108@gmail.com>
2024-05-21 15:22:03 +0900

    Update fly.toml & healthcheck.tsx

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,25 +1,30 @@
-// learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderFunctionArgs } from "@remix-run/node";
-
 import { prisma } from "~/db.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+	const host =
+		request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
 
-  try {
-    const url = new URL("/", `http://${host}`);
-    // if we can connect to the database and make a simple query
-    // and make a HEAD request to ourselves, then we're good.
-    await Promise.all([
-      prisma.user.count(),
-      fetch(url.toString(), { method: "HEAD" }).then((r) => {
-        if (!r.ok) return Promise.reject(r);
-      }),
-    ]);
-    return new Response("OK");
-  } catch (error: unknown) {
-    console.log("healthcheck ❌", { error });
-    return new Response("ERROR", { status: 500 });
-  }
+	console.log("Healthcheck initiated");
+
+	try {
+		const url = new URL("/", `http://${host}`);
+		console.log(`Connecting to the database at host: ${host}`);
+
+		// Perform a simple query to check the database connection
+		const userCount = await prisma.user.count();
+		console.log(`User count: ${userCount}`);
+
+		console.log(`Making a HEAD request to URL: ${url.toString()}`);
+		await fetch(url.toString(), { method: "HEAD" }).then((r) => {
+			if (!r.ok) return Promise.reject(r);
+			console.log("HEAD request successful");
+		});
+
+		console.log("Healthcheck passed");
+		return new Response("OK");
+	} catch (error: unknown) {
+		console.error("Healthcheck ❌", { error });
+		return new Response("ERROR", { status: 500 });
+	}
 };
